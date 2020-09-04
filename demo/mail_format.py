f44b22da24290feaa580a53d684b20ff329a2c80
never <lyyenjoyparkchoah@126.com>
2020-09-04 18:54:58 +0800

    2020/09/04/18/54

--- demo/mail_format.py
+++ demo/mail_format.py
@@ -6,17 +6,16 @@
             每个字段（Field）包括两部分：
                 字段名 字段值（冒号分隔）
                     字段：
-                        Return-Path：该字段由信息的最后发送者添加,是关于信息原始来源的地址和回朔路径
-                        Received：由每个中继服务站添加，用于帮助追踪传输中出现的错误
-                        Date：
-                        From：邮件作者（存在多个作者必须指定Sender字段）（Reply-To字段存在会使用该字段，而不会使用From）
-                        Subject：
-                        Sender：邮件发送者
-                        To：
-                        Cc：
-                        MIME-Version：
-                        Content-type：
-                        Bcc：
+                        Return-Path：表示退信的地址
+                        Received：表示路由信息
+                        Date：表示建立信件的时间
+                        From：表示邮件作者（存在多个作者必须指定Sender字段）（Reply-To字段存在会使用该字段，而不会使用From）
+                        Subject：邮件的主题
+                        Sender：表示邮件的实际发送者
+                        To：表示接受的邮件地址
+                        cc：表示接受的邮件地址
+                        MIME-Version：表示MIME的版本
+                        Content-type：邮件内容的格式
 
         邮件体：
             邮件的内容（类型由邮件头Content-Type字段）（RFC 2822邮件格式，邮件体是ASCII编码的字符序列）
